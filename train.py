9f70a61454ea1a1961fe8206b886d4a68aa57bcb
Likarian <likarian1207@gmail.com>
2019-06-24 01:15:47 +0900

    test

--- train.py
+++ train.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import VDSR_model
 import generator
 import argparse
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 from keras.callbacks import CSVLogger
 import tensorflow as tf
 from keras import backend as K
